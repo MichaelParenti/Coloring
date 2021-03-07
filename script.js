c75f376e79f744f50aabc5ca8483b09c9460bfa3
Michael Parenti <78945650+M-Coder-3920@users.noreply.github.com>
2021-03-07 13:48:27 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,708 @@
+function changeA(){
+
+    var color = document.getElementById("color").value;
+    var a = document.getElementById("a");
+
+    a.style.backgroundColor = color;
+}
+function changeB(){
+
+    var color = document.getElementById("color").value;
+    var b = document.getElementById("b");
+
+    b.style.backgroundColor = color;
+}
+function changeC(){
+
+    var color = document.getElementById("color").value;
+    var c = document.getElementById("c");
+
+    c.style.backgroundColor = color;
+}
+function changeD(){
+
+    var color = document.getElementById("color").value;
+    var d = document.getElementById("d");
+
+    d.style.backgroundColor = color;
+}
+function changeE(){
+
+    var color = document.getElementById("color").value;
+    var e = document.getElementById("e");
+
+    e.style.backgroundColor = color;
+}
+function changeF(){
+
+    var color = document.getElementById("color").value;
+    var f = document.getElementById("f");
+
+    f.style.backgroundColor = color;
+}
+function changeG(){
+
+    var color = document.getElementById("color").value;
+    var g = document.getElementById("g");
+
+    g.style.backgroundColor = color;
+}
+function changeH(){
+
+    var color = document.getElementById("color").value;
+    var h = document.getElementById("h");
+
+    h.style.backgroundColor = color;
+}
+function changeI(){
+
+    var color = document.getElementById("color").value;
+    var i = document.getElementById("i");
+
+    i.style.backgroundColor = color;
+}
+function changeJ(){
+
+    var color = document.getElementById("color").value;
+    var j = document.getElementById("j");
+
+    j.style.backgroundColor = color;
+}
+
+
+
+
+function changeA2(){
+
+    var color = document.getElementById("color").value;
+    var a2 = document.getElementById("a2");
+
+    a2.style.backgroundColor = color;
+}
+function changeB2(){
+
+    var color = document.getElementById("color").value;
+    var b2 = document.getElementById("b2");
+
+    b2.style.backgroundColor = color;
+}
+function changeC2(){
+
+    var color = document.getElementById("color").value;
+    var c2 = document.getElementById("c2");
+
+    c2.style.backgroundColor = color;
+}
+function changeD2(){
+
+    var color = document.getElementById("color").value;
+    var d2 = document.getElementById("d2");
+
+    d2.style.backgroundColor = color;
+}
+function changeE2(){
+
+    var color = document.getElementById("color").value;
+    var e2 = document.getElementById("e2");
+
+    e2.style.backgroundColor = color;
+}
+function changeF2(){
+
+    var color = document.getElementById("color").value;
+    var f2 = document.getElementById("f2");
+
+    f2.style.backgroundColor = color;
+}
+function changeG2(){
+
+    var color = document.getElementById("color").value;
+    var g2 = document.getElementById("g2");
+
+    g2.style.backgroundColor = color;
+}
+function changeH2(){
+
+    var color = document.getElementById("color").value;
+    var h2 = document.getElementById("h2");
+
+    h2.style.backgroundColor = color;
+}
+function changeI2(){
+
+    var color = document.getElementById("color").value;
+    var i2 = document.getElementById("i2");
+
+    i2.style.backgroundColor = color;
+}
+function changeJ2(){
+
+    var color = document.getElementById("color").value;
+    var j2 = document.getElementById("j2");
+
+    j2.style.backgroundColor = color;
+}
+
+
+
+
+function changeA3(){
+
+    var color = document.getElementById("color").value;
+    var a3 = document.getElementById("a3");
+
+    a3.style.backgroundColor = color;
+}
+function changeB3(){
+
+    var color = document.getElementById("color").value;
+    var b3 = document.getElementById("b3");
+
+    b3.style.backgroundColor = color;
+}
+function changeC3(){
+
+    var color = document.getElementById("color").value;
+    var c3 = document.getElementById("c3");
+
+    c3.style.backgroundColor = color;
+}
+function changeD3(){
+
+    var color = document.getElementById("color").value;
+    var d3 = document.getElementById("d3");
+
+    d3.style.backgroundColor = color;
+}
+function changeE3(){
+
+    var color = document.getElementById("color").value;
+    var e3 = document.getElementById("e3");
+
+    e3.style.backgroundColor = color;
+}
+function changeF3(){
+
+    var color = document.getElementById("color").value;
+    var f3 = document.getElementById("f3");
+
+    f3.style.backgroundColor = color;
+}
+function changeG3(){
+
+    var color = document.getElementById("color").value;
+    var g3 = document.getElementById("g3");
+
+    g3.style.backgroundColor = color;
+}
+function changeH3(){
+
+    var color = document.getElementById("color").value;
+    var h3 = document.getElementById("h3");
+
+    h3.style.backgroundColor = color;
+}
+function changeI3(){
+
+    var color = document.getElementById("color").value;
+    var i3 = document.getElementById("i3");
+
+    i3.style.backgroundColor = color;
+}
+function changeJ3(){
+
+    var color = document.getElementById("color").value;
+    var j3 = document.getElementById("j3");
+
+    j3.style.backgroundColor = color;
+}
+function changeA4(){
+
+    var color = document.getElementById("color").value;
+    var a4 = document.getElementById("a4");
+
+    a4.style.backgroundColor = color;
+}
+function changeB4(){
+
+    var color = document.getElementById("color").value;
+    var b4 = document.getElementById("b4");
+
+    b4.style.backgroundColor = color;
+}
+function changeC4(){
+
+    var color = document.getElementById("color").value;
+    var c4 = document.getElementById("c4");
+
+    c4.style.backgroundColor = color;
+}
+function changeD4(){
+
+    var color = document.getElementById("color").value;
+    var d4 = document.getElementById("d4");
+
+    d4.style.backgroundColor = color;
+}
+function changeE4(){
+
+    var color = document.getElementById("color").value;
+    var e4 = document.getElementById("e4");
+
+    e4.style.backgroundColor = color;
+}
+function changeF4(){
+
+    var color = document.getElementById("color").value;
+    var f4 = document.getElementById("f4");
+
+    f4.style.backgroundColor = color;
+}
+function changeG4(){
+
+    var color = document.getElementById("color").value;
+    var g4 = document.getElementById("g4");
+
+    g4.style.backgroundColor = color;
+}
+function changeH4(){
+
+    var color = document.getElementById("color").value;
+    var h4 = document.getElementById("h4");
+
+    h4.style.backgroundColor = color;
+}
+function changeI4(){
+
+    var color = document.getElementById("color").value;
+    var i4 = document.getElementById("i4");
+
+    i4.style.backgroundColor = color;
+}
+function changeJ4(){
+
+    var color = document.getElementById("color").value;
+    var j4 = document.getElementById("j4");
+
+    j4.style.backgroundColor = color;
+}
+function changeA5(){
+
+    var color = document.getElementById("color").value;
+    var a5 = document.getElementById("a5");
+
+    a5.style.backgroundColor = color;
+}
+function changeB5(){
+
+    var color = document.getElementById("color").value;
+    var b5 = document.getElementById("b5");
+
+    b5.style.backgroundColor = color;
+}
+function changeC5(){
+
+    var color = document.getElementById("color").value;
+    var c5 = document.getElementById("c5");
+
+    c5.style.backgroundColor = color;
+}
+function changeD5(){
+
+    var color = document.getElementById("color").value;
+    var d5 = document.getElementById("d5");
+
+    d5.style.backgroundColor = color;
+}
+function changeE5(){
+
+    var color = document.getElementById("color").value;
+    var e5 = document.getElementById("e5");
+
+    e5.style.backgroundColor = color;
+}
+function changeF5(){
+
+    var color = document.getElementById("color").value;
+    var f5 = document.getElementById("f5");
+
+    f5.style.backgroundColor = color;
+}
+function changeG5(){
+
+    var color = document.getElementById("color").value;
+    var g5 = document.getElementById("g5");
+
+    g5.style.backgroundColor = color;
+}
+function changeH5(){
+
+    var color = document.getElementById("color").value;
+    var h5 = document.getElementById("h5");
+
+    h5.style.backgroundColor = color;
+}
+function changeI5(){
+
+    var color = document.getElementById("color").value;
+    var i5 = document.getElementById("i5");
+
+    i5.style.backgroundColor = color;
+}
+function changeJ5(){
+
+    var color = document.getElementById("color").value;
+    var j5 = document.getElementById("j5");
+
+    j5.style.backgroundColor = color;
+}
+function changeA6(){
+
+    var color = document.getElementById("color").value;
+    var a6 = document.getElementById("a6");
+
+    a6.style.backgroundColor = color;
+}
+function changeB6(){
+
+    var color = document.getElementById("color").value;
+    var b6 = document.getElementById("b6");
+
+    b6.style.backgroundColor = color;
+}
+function changeC6(){
+
+    var color = document.getElementById("color").value;
+    var c6 = document.getElementById("c6");
+
+    c6.style.backgroundColor = color;
+}
+function changeD6(){
+
+    var color = document.getElementById("color").value;
+    var d6 = document.getElementById("d6");
+
+    d6.style.backgroundColor = color;
+}
+function changeE6(){
+
+    var color = document.getElementById("color").value;
+    var e6 = document.getElementById("e6");
+
+    e6.style.backgroundColor = color;
+}
+function changeF6(){
+
+    var color = document.getElementById("color").value;
+    var f6 = document.getElementById("f6");
+
+    f6.style.backgroundColor = color;
+}
+function changeG6(){
+
+    var color = document.getElementById("color").value;
+    var g6 = document.getElementById("g6");
+
+    g6.style.backgroundColor = color;
+}
+function changeH6(){
+
+    var color = document.getElementById("color").value;
+    var h6 = document.getElementById("h6");
+
+    h6.style.backgroundColor = color;
+}
+function changeI6(){
+
+    var color = document.getElementById("color").value;
+    var i6 = document.getElementById("i6");
+
+    i6.style.backgroundColor = color;
+}
+function changeJ6(){
+
+    var color = document.getElementById("color").value;
+    var j6 = document.getElementById("j6");
+
+    j6.style.backgroundColor = color;
+}
+function changeA7(){
+
+    var color = document.getElementById("color").value;
+    var a7 = document.getElementById("a7");
+
+    a7.style.backgroundColor = color;
+}
+function changeB7(){
+
+    var color = document.getElementById("color").value;
+    var b7 = document.getElementById("b7");
+
+    b7.style.backgroundColor = color;
+}
+function changeC7(){
+
+    var color = document.getElementById("color").value;
+    var c7 = document.getElementById("c7");
+
+    c7.style.backgroundColor = color;
+}
+function changeD7(){
+
+    var color = document.getElementById("color").value;
+    var d7 = document.getElementById("d7");
+
+    d7.style.backgroundColor = color;
+}
+function changeE7(){
+
+    var color = document.getElementById("color").value;
+    var e7 = document.getElementById("e7");
+
+    e7.style.backgroundColor = color;
+}
+function changeF7(){
+
+    var color = document.getElementById("color").value;
+    var f7 = document.getElementById("f7");
+
+    f7.style.backgroundColor = color;
+}
+function changeG7(){
+
+    var color = document.getElementById("color").value;
+    var g7 = document.getElementById("g7");
+
+    g7.style.backgroundColor = color;
+}
+function changeH7(){
+
+    var color = document.getElementById("color").value;
+    var h7 = document.getElementById("h7");
+
+    h7.style.backgroundColor = color;
+}
+function changeI7(){
+
+    var color = document.getElementById("color").value;
+    var i7 = document.getElementById("i7");
+
+    i7.style.backgroundColor = color;
+}
+function changeJ7(){
+
+    var color = document.getElementById("color").value;
+    var j7 = document.getElementById("j7");
+
+    j7.style.backgroundColor = color;
+}
+function changeA8(){
+
+    var color = document.getElementById("color").value;
+    var a8 = document.getElementById("a8");
+
+    a8.style.backgroundColor = color;
+}
+function changeB8(){
+
+    var color = document.getElementById("color").value;
+    var b8 = document.getElementById("b8");
+
+    b8.style.backgroundColor = color;
+}
+function changeC8(){
+
+    var color = document.getElementById("color").value;
+    var c8 = document.getElementById("c8");
+
+    c8.style.backgroundColor = color;
+}
+function changeD8(){
+
+    var color = document.getElementById("color").value;
+    var d8 = document.getElementById("d8");
+
+    d8.style.backgroundColor = color;
+}
+function changeE8(){
+
+    var color = document.getElementById("color").value;
+    var e8 = document.getElementById("e8");
+
+    e8.style.backgroundColor = color;
+}
+function changeF8(){
+
+    var color = document.getElementById("color").value;
+    var f8 = document.getElementById("f8");
+
+    f8.style.backgroundColor = color;
+}
+function changeG8(){
+
+    var color = document.getElementById("color").value;
+    var g8 = document.getElementById("g8");
+
+    g8.style.backgroundColor = color;
+}
+function changeH8(){
+
+    var color = document.getElementById("color").value;
+    var h8 = document.getElementById("h8");
+
+    h8.style.backgroundColor = color;
+}
+function changeI8(){
+
+    var color = document.getElementById("color").value;
+    var i8 = document.getElementById("i8");
+
+    i8.style.backgroundColor = color;
+}
+function changeJ8(){
+
+    var color = document.getElementById("color").value;
+    var j8 = document.getElementById("j8");
+
+    j8.style.backgroundColor = color;
+}
+function changeA9(){
+
+    var color = document.getElementById("color").value;
+    var a9 = document.getElementById("a9");
+
+    a9.style.backgroundColor = color;
+}
+function changeB9(){
+
+    var color = document.getElementById("color").value;
+    var b9 = document.getElementById("b9");
+
+    b9.style.backgroundColor = color;
+}
+function changeC9(){
+
+    var color = document.getElementById("color").value;
+    var c9 = document.getElementById("c9");
+
+    c9.style.backgroundColor = color;
+}
+function changeD9(){
+
+    var color = document.getElementById("color").value;
+    var d9 = document.getElementById("d9");
+
+    d9.style.backgroundColor = color;
+}
+function changeE9(){
+
+    var color = document.getElementById("color").value;
+    var e9 = document.getElementById("e9");
+
+    e9.style.backgroundColor = color;
+}
+function changeF9(){
+
+    var color = document.getElementById("color").value;
+    var f9 = document.getElementById("f9");
+
+    f9.style.backgroundColor = color;
+}
+function changeG9(){
+
+    var color = document.getElementById("color").value;
+    var g9 = document.getElementById("g9");
+
+    g9.style.backgroundColor = color;
+}
+function changeH9(){
+
+    var color = document.getElementById("color").value;
+    var h9 = document.getElementById("h9");
+
+    h9.style.backgroundColor = color;
+}
+function changeI9(){
+
+    var color = document.getElementById("color").value;
+    var i9 = document.getElementById("i9");
+
+    i9.style.backgroundColor = color;
+}
+function changeJ9(){
+
+    var color = document.getElementById("color").value;
+    var j9 = document.getElementById("j9");
+
+    j9.style.backgroundColor = color;
+}
+function changeA10(){
+
+    var color = document.getElementById("color").value;
+    var a10 = document.getElementById("a10");
+
+    a10.style.backgroundColor = color;
+}
+function changeB10(){
+
+    var color = document.getElementById("color").value;
+    var b10 = document.getElementById("b10");
+
+    b10.style.backgroundColor = color;
+}
+function changeC10(){
+
+    var color = document.getElementById("color").value;
+    var c10 = document.getElementById("c10");
+
+    c10.style.backgroundColor = color;
+}
+function changeD10(){
+
+    var color = document.getElementById("color").value;
+    var d10 = document.getElementById("d10");
+
+    d10.style.backgroundColor = color;
+}
+function changeE10(){
+
+    var color = document.getElementById("color").value;
+    var e10 = document.getElementById("e10");
+
+    e10.style.backgroundColor = color;
+}
+function changeF10(){
+
+    var color = document.getElementById("color").value;
+    var f10 = document.getElementById("f10");
+
+    f10.style.backgroundColor = color;
+}
+function changeG10(){
+
+    var color = document.getElementById("color").value;
+    var g10 = document.getElementById("g10");
+
+    g10.style.backgroundColor = color;
+}
+function changeH10(){
+
+    var color = document.getElementById("color").value;
+    var h10 = document.getElementById("h10");
+
+    h10.style.backgroundColor = color;
+}
+function changeI10(){
+
+    var color = document.getElementById("color").value;
+    var i10 = document.getElementById("i10");
+
+    i10.style.backgroundColor = color;
+}
+function changeJ10(){
+
+    var color = document.getElementById("color").value;
+    var j10 = document.getElementById("j10");
+
+    j10.style.backgroundColor = color;
+}
